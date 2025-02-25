1f81a19d2072e4d56cbc0ebe52ea82313d44b778
Guilhem Vellut <g@vellut.com>
2025-02-25 19:22:12 +0100

    Waiting for video + label

--- yt_speed.js
+++ yt_speed.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Easier video speed setting
-// @namespace    http://tampermonkey.net/
+// @namespace    https://vellut.com/
 // @version      0.4
 // @description  It's all in the title
-// @author       RaresGeo
+// @author       GV
 // @match        *://www.youtube.com/*
 // @grant        none
 // ==/UserScript==
@@ -13,18 +13,67 @@
     const hotkeys = {
         DECREASE: "KeyZ",
         RESET: "KeyX",
-        INCREASE: "KeyC",
+        SPEED: "KeyC",
         MAX: "KeyV",
     };
 
 
     const setSpeed = (speed) => {
-        let videos = document.getElementsByClassName("html5-main-video");
-        for (let i = 0; i < videos.length; i++) {
-            videos[i].playbackRate = speed;
-        }
-        console.log(`New speed is ${speed}x`);
+        waitFor(".html5-main-video").then((video) => {
+            video.playbackRate = speed;
+
+            console.log(`New speed is ${speed}x`);
+
+            let oldDisplay = document.getElementById("speed-display-monkey");
+            if (!oldDisplay) {
+                let newElement = document.createElement("div");
+                let display = `<div id="speed-display-monkey" style="border: 2px solid white; position: absolute; left: 2.5%; top: 50px; width: fit-content; height: fit-content; z-index: 9999; background: rgba(15, 30, 18, .5); cursor: all-scroll; padding: 1rem; border-radius: 5px;"><strong style="color: white; font-size: 22px;">${speed}X speed</strong></div>`;
+                newElement.innerHTML = display;
+
+                document.body.append(newElement);
+            } else {
+                oldDisplay.getElementsByTagName("strong")[0].innerText = `${speed}X speed`;
+                oldDisplay.style.opacity = 1;
+            }
+            fadeOutEffect();
+        });
     };
+
+    function waitFor(selector) {
+        return new Promise(resolve => {
+            if (document.querySelector(selector)) {
+                return resolve(document.querySelector(selector));
+            }
+
+            const observer = new MutationObserver(mutations => {
+                if (document.querySelector(selector)) {
+                    observer.disconnect();
+                    resolve(document.querySelector(selector));
+                }
+            });
+
+            // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
+        });
+    }
+
+    function fadeOutEffect() {
+        var fadeTarget = document.getElementById("speed-display-monkey");
+        var fadeEffect = setInterval(function () {
+            if (!fadeTarget.style.opacity) {
+                fadeTarget.style.opacity = 1;
+            }
+            if (fadeTarget.style.opacity > 0) {
+                fadeTarget.style.opacity -= 0.1;
+            } else {
+                clearInterval(fadeEffect);
+            }
+        }, 200);
+    }
+
 
     const afterNavigate = () => {
         if ("/watch" === location.pathname) {
@@ -52,7 +101,7 @@
                 case hotkeys.RESET:
                     speed = 1;
                     break;
-                case hotkeys.INCREASE:
+                case hotkeys.SPEED:
                     speed = 1.75;
                     break;
                 case hotkeys.MAX:
